85a889244560716fdfacd6135e8fd258ad8b6461
Krystian Carter <krystian.carter18@gmail.com>
2024-08-21 14:47:34 -0400

    Will soon become helper classes once I rewrite the gui system.

---
+++ src/main/java/org/mystichorizons/vaultHunters/gui/types/Inventory.java
@@ -0,0 +1,7 @@
+package org.mystichorizons.vaultHunters.gui.types;
+
+public class Inventory {
+
+    //TODO: This class will be a handler for the Inventory GUI type to allow for custom inventories
+
+}
---
+++ src/main/java/org/mystichorizons/vaultHunters/gui/types/InventoryDrop.java
@@ -0,0 +1,7 @@
+package org.mystichorizons.vaultHunters.gui.types;
+
+public class InventoryDrop {
+
+    // TODO: This class will be a handler for the InventoryDrop GUI type to allow item drops in a certain area of the inventory
+
+}
